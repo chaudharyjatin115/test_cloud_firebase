bf7776077926a03bbfbae6da2e4624286ea19969
Jatin Chaudhary <chaudharyjatin115@gmail.com>
2022-06-29 10:51:19 +0530

    adding some dialogs

--- lib/dialogs/delete_account_dialog.dart
+++ lib/dialogs/delete_account_dialog.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show BuildContext;
 import 'package:test_cloud_firebase/dialogs/generic_dialogs.dart';
-
-Future<bool> showDeleteAccountDialog(BuildContext context) {
-  return showGenericDialog(
+// 
+Future<bool?> showDeleteAccountDialog(BuildContext context) {
+  return showGenericDialog<bool>(
       context: context,
       title: 'Delete Account',
       content: 'are you sure you want to delete your account?',
       optionsBuilder: () => {
+        // key        value
             'cancel': false,
             'Delete account': true,
           }).then((value) => value ?? false);
